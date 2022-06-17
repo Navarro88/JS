7e7312d101cffda5f7cbfafb22021b9bca5052ab
Navarro88 <104008422+Navarro88@users.noreply.github.com>
2022-06-17 17:53:21 +0300

    Update Hw_JS.js

--- Hw_JS.js
+++ Hw_JS.js
@@ -27,11 +27,12 @@ let age_1 = 16
 let age_2 = 11
 let age_3 = 22
 
-if(age_1 < age_2)
+if(age_1 < age_2) {
 console.log("You don’t have access cause your age is " + age_1 + " It’s less then")
-else if(age_1 >= age_2 && age_1 < age_3)
+} else if(age_1 >= age_2 && age_1 < age_3) {
 console.log("Welcome!")
-else if(age_1 > age_3)
+} else if(age_1 > age_3) {
 console.log("Keep calm and look Culture chanell")
-else 
+} else {
 console.log("Technical work")
+}
